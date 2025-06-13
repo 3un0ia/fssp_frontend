05a0646cb88a77ff281b158dc88cf45c93bdf2c0
3un0ia <kj122kj@naver.com>
2025-06-14 01:38:21 +0900

    add settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/auth_provider.dart';
+import 'services/api_client.dart';
+import 'services/auth_api.dart';
+import 'services/movie_api.dart';
+import 'services/watchlist_api.dart';
 import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/genre_selection_screen.dart';
+
+final RouteObserver<ModalRoute<void>> routeObserver = RouteObserver<ModalRoute<void>>();
 
 void main() {
+  final apiClient = ApiClient(baseUrl: "http://127.0.0.1:8080");
+  final authApi = AuthApi(client: apiClient);
+  final movieApi = MovieApi(client: apiClient);
+  final watchlistApi = WatchlistApi(client: apiClient);
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
@@ -10,7 +26,20 @@ void main() {
       statusBarIconBrightness: Brightness.light,
     ),
   );
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<ApiClient>.value(value: apiClient),
+        Provider<MovieApi>.value(value: movieApi),
+        Provider<AuthApi>.value(value: authApi),
+        Provider<WatchlistApi>.value(value: watchlistApi),
+        ChangeNotifierProvider<AuthProvider>(
+          create: (_) => AuthProvider(authApi: authApi, apiClient: apiClient),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +47,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Movie App',
+      navigatorObservers: [routeObserver],
+      initialRoute: '/login',
+      routes: {
+        '/login' : (_) => const LoginScreen(),
+        '/home' : (_) => const HomeScreen(),
+        '/signup' : (_) => const RegisterScreen(),
+        '/genre-selection' : (_) => GenreSelectionScreen(),
+      },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF1C2732),
@@ -27,8 +64,8 @@ class MyApp extends StatelessWidget {
           elevation: 0,
         ),
         textTheme: TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-          bodyText2: TextStyle(color: Colors.white),
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
         ),
         inputDecorationTheme: InputDecorationTheme(
           filled: true,
@@ -40,7 +77,7 @@ class MyApp extends StatelessWidget {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            primary: Color(0xFF0A84FF),
+            backgroundColor: Color(0xFF0A84FF),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
